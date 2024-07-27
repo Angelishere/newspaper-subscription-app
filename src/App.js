56adb12af7c11dcd5f5fe56d000d4615453aec4e
Angelishere <angelsara1804@gmail.com>
2024-07-27 23:13:19 +0530

    Delete subscriber added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddSubscriber from './components/AddSubscriber';
 import SearchSubscriber from './components/SearchSubscriber';
+import DeleteSubscriber from './components/DeleteSubscriber';
 
 function App() {
   return (
     <div>
       <AddSubscriber/>
       <SearchSubscriber/>
+      <DeleteSubscriber/>
     </div>
   );
 }
