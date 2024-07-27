bb9a0cc690178dc927584a59b35ef0ba2d104ea8
Angelishere <angelsara1804@gmail.com>
2024-07-27 23:05:41 +0530

    Search subcriber name added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSubscriber from './components/AddSubscriber';
+import SearchSubscriber from './components/SearchSubscriber';
 
 function App() {
   return (
     <div>
       <AddSubscriber/>
+      <SearchSubscriber/>
     </div>
   );
 }
